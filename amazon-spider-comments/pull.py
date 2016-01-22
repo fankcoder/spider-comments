8cf2a8bee5dce724b03eb7cfdce10b73eeae0a50
icgoo <zf.ice@163.com>
2016-01-22 16:59:51 +0800

    strong amazon-spider

--- amazon-spider-comments/pull.py
+++ amazon-spider-comments/pull.py
@@ -3,9 +3,10 @@ import requests
 import re
 import json
 from bs4 import BeautifulSoup
+import time
 import sys
-reload(sys)  
-sys.setdefaultencoding('utf8')
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def downWeb(url,page):
     user_agent = ("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0")
@@ -21,12 +22,39 @@ def downWeb(url,page):
                 'Upgrade-Insecure-Requests':'1'
                 }
     contentlist = []
-    for each in range(0,page):
+    for each in range(231,page):
         realpage = each +1
-        print "dowing page",realpage,"now ..."
         realUrl = url %(realpage, realpage)
-        r = requests.get(realUrl,headers=headers)
-        contentlist.append(r.content)
+        try:
+            time.sleep(2)
+            r = requests.get(realUrl, headers=headers)
+            soup = BeautifulSoup(r.content)
+            comments = soup.find(id="cm_cr-review_list")
+            if type(comments) == type(None):
+                print "try page",realpage,"again..."
+                time.sleep(3)
+                r = requests.get(realUrl, headers=headers)
+                soup = BeautifulSoup(r.content)
+                comments = soup.find(id="cm_cr-review_list")
+                if type(comments) == type(None):
+                    print "try page",realpage,"again..."
+                    time.sleep(5)
+                    r = requests.get(realUrl, headers=headers)
+                    soup = BeautifulSoup(r.content)
+                    comments = soup.find(id="cm_cr-review_list")
+                    if type(comments) == type(None):
+                        print "page ",realpage,"give up !!!!"
+                    else:
+                        print "page",realpage,"downloding successful..."
+                        contentlist.append(r.content)
+                else:
+                    print "page",realpage,"downloding successful..."
+                    contentlist.append(r.content)
+            else:
+                print "page",realpage,"downloding successful..."
+                contentlist.append(r.content)
+        except requests.exceptions.RequestException as e:
+            print e
     return contentlist
 
 def matchRe(contentlist):
@@ -37,27 +65,37 @@ def matchRe(contentlist):
     text = []
     tanslate = ""
     date = []
+    num = 1
     
     for each in contentlist:
-        print "10 comments downloading..."
         content = each
         soup = BeautifulSoup(content)
-        try:
-            comments = soup.find(id="cm_cr-review_list")
-            comment = comments.find_all("div","a-section review")
-            for each in comment:
-                rate.append(each.i.span.get_text().split()[0].decode().encode('utf-8'))
-                title.append(each.find_all("a", class_="review-title")[0].get_text().decode('utf-8'))
-                author.append(each.find_all("a", class_="author")[0].get_text().decode('utf-8'))
-                text.append(each.find_all("span", class_="review-text")[0].get_text().decode('utf-8'))
-                date.append(each.find_all("span", class_="review-date")[0].get_text().decode().encode('utf-8'))
-        except:
-            print 'data error...'
+
+        comments = soup.find(id="cm_cr-review_list")
+        comment = comments.find_all("div","a-section review")
+        #print "put in page",num,"comment"
+
+        for each in comment:
+            eachrate = each.i.span.get_text().split()[0].decode().encode('utf-8')
+            rate.append(eachrate)
+            #rate.append(each.i.span.get_text().split()[0].decode().encode('utf-8'))
+            eachtitle = each.find_all("a", class_="review-title")[0].get_text().decode('utf-8')
+            title.append(repr(eachtitle)[2:-1])
+            #title.append(each.find_all("a", class_="review-title")[0].get_text().decode('utf-8'))
+            eachauthor = each.find_all("a", class_="author")[0].get_text().decode('utf-8')
+            author.append(repr(eachauthor)[2:-1])
+            #author.append(each.find_all("a", class_="author")[0].get_text().decode('utf-8'))
+            eachtext = (each.find_all("span", class_="review-text")[0].get_text().decode('utf-8'))
+            text.append(repr(eachtext)[2:-1])
+            eachdate = each.find_all("span", class_="review-date")[0].get_text().decode().encode('utf-8')
+            date.append(eachdate)
+            #date.append(each.find_all("span", class_="review-date")[0].get_text().decode().encode('utf-8'))
+            #print repr(eachrate),repr(eachtitle),repr(eachauthor),repr(eachtext),repr(eachdate)
 
     commentDict =  dict(system=system,rate=rate,
                         title=title,author=author,text=text,
                         tanslate=tanslate,date=date)
-    print "All comments download successful..."
+    print len(author),"comments download successful..."
     return commentDict
 
 def saveToExcel(comment):
@@ -79,15 +117,18 @@ def saveToExcel(comment):
 
     for num,each in enumerate(rate):
         index = num +1
-        table.write(index,0,system)
-        table.write(index,1,rate[num])
-        table.write(index,2,title[num])
-        table.write(index,3,author[num])
-        table.write(index,4,text[num])
-        table.write(index,5,tanslate)
-        table.write(index,6,date[num])
+        try:
+            table.write(index,0,system)
+            table.write(index,1,rate[num])
+            table.write(index,2,title[num])
+            table.write(index,3,author[num])
+            table.write(index,4,text[num])
+            table.write(index,5,tanslate)
+            table.write(index,6,date[num])
+        except:
+            print "len error or ascii error"
 
-    efile.save('crush.xls')
+    efile.save('wordFall.xls')
     print "Save data successful..."
 
 def savefile(content):
@@ -96,9 +137,11 @@ def savefile(content):
     f.close()
 
 if __name__ == '__main__':
-    page = 87
-    url = "http://www.amazon.com/Crush-Letters-Challenging-Search-Puzzle/product-reviews/B00JPVPX2A/ref=cm_cr_pr_btm_link_%d?ie=UTF8&amp%%3BshowViewpoints=1&amp%%3BsortBy=recent&amp%%3BpageNumber=9&pageNumber=%d"
-    content = downWeb(url,page)
+    page = 232
+    Crushurl = "http://www.amazon.com/Crush-Letters-Challenging-Search-Puzzle/product-reviews/B00JPVPX2A/ref=cm_cr_pr_btm_link_%d?ie=UTF8&amp%%3BshowViewpoints=1&amp%%3BsortBy=recent&amp%%3BpageNumber=9&pageNumber=%d"
+    WordFallurl = "http://www.amazon.com/WordFall-Addictive-Words-Search-Puzzle/product-reviews/B016EX8NK0/ref=cm_cr_pr_btm_link_%d?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber=%d"
+    WordJungle ="http://www.amazon.com/Word-Jungle-Challenging-Brain-Puzzle/product-reviews/B015II9BBC/ref=cm_cr_pr_btm_link_%d?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber=%d"
+    content = downWeb(WordFallurl,page)
     comments = matchRe(content)
     savefile = saveToExcel(comments)
     #savefile(comments)
--- test.py
+++
@@ -1,5 +0,0 @@
-import cookielib
-filename ='hello.txt'
-cookie = cookielib.MozillaCookieJar(filename)
-cookie.save(ignore_discard=True, ignore_expires=True)
-
